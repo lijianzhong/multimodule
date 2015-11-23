5a2886dc5d2d3bc6a4c41fa63805608cb99060de
李建忠 <lijianzhong@meituan.com>
2015-11-23 15:01:52 +0800

    init
    test mulitModule

---
+++ module1/src/main/java/com/example/module1/Test1Class.java
@@ -0,0 +1,11 @@
+package com.example.module1;
+
+/**
+ * Created by jianzhongli on 15/11/23.
+ */
+public class Test1Class {
+
+    public void doSomething(){
+
+    }
+}
---
+++ module2/src/main/java/com/example/module2/Test2Class.java
@@ -0,0 +1,11 @@
+package com.example.module2;
+
+/**
+ * Created by jianzhongli on 15/11/23.
+ */
+public class Test2Class {
+
+    public void doSomething(){
+
+    }
+}
